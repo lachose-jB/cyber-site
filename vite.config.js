2040d9a91fb5211bb6a21c1a9ae8b5f838a17811
azeto <azeto@localhost.localdomain>
2024-12-13 03:19:41 +0100

    first

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jean-Baptiste-portofolio/',
+  base: '/jean-Baptiste-portfolio/', 
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
+        additionalData: '',
         silenceDeprecations: ['mixed-decls'],
       },
     },
