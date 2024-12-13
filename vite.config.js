a0767defdf700983a0b8b4bb29d4f04be3fac732
azeto <azeto@localhost.localdomain>
2024-12-13 03:49:22 +0100

    first

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import csp from 'vite-plugin-csp'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jean-Baptiste-portfolio/', 
-  plugins: [react()],
+  base: '/jean-Baptiste-portfolio/',
+  plugins: [
+    react(),
+    csp({
+      policy: {
+        'default-src': ["'self'"],
+        'script-src': ["'self'", "'unsafe-inline'"],
+        'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
+        'font-src': ["'self'", 'https://fonts.gstatic.com'],
+        'img-src': ["'self'", 'data:'],
+        'connect-src': ["'self'", 'https://api.example.com'],
+        'frame-src': ["'none'"],
+        'object-src': ["'none'"]
+      },
+      // Active le mode strict si nécessaire
+      strict: true,
+    })
+  ],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: '',
         silenceDeprecations: ['mixed-decls'],
       },
     },
